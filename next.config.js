c912544524f622ec5942b895bcabf2c081728177
ahung888 <yunhung2000@gmail.com>
2021-11-08 23:38:11 +0800

    rename keyname

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    APP_VERSION: '0.1.5',
-    MAPBOX_TOKEN: process.env.REACT_APP_MAP_API,
-    PTX_API_ID: process.env.REACT_APP_PTX_API_ID,
-    PTX_API_KEY: process.env.REACT_APP_PTX_API_KEY,
+    APP_VERSION: '0.1.6',
+    MAPBOX_TOKEN: process.env.MAP_TOKEN,
+    PTX_API_ID: process.env.PTX_API_ID,
+    PTX_API_KEY: process.env.PTX_API_KEY,
   },
   basePath: '/taiwan-tourist-attraction-map',
 }
