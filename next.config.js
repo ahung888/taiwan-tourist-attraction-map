eac89c695b7d955576af36fbd529fa5fbebc721d
ahung888 <yunhung2000@gmail.com>
2021-11-08 22:09:52 +0800

    try deploy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    APP_VERSION: '0.1.2',
+    APP_VERSION: '0.1.3',
     MAPBOX_TOKEN: process.env.REACT_APP_MAP_API,
     PTX_API_ID: process.env.PTX_API_ID,
     PTX_API_KEY: process.env.PTX_API_KEY,
