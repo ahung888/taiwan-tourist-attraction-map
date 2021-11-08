284f4c1e0507ede4ffaceffc881035ee215f3c72
ahung888 <yunhung2000@gmail.com>
2021-11-08 11:10:48 +0800

    add relative path for images & add dev version

--- components/Nav.js
+++ components/Nav.js
@@ -1,16 +1,31 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 import {GiBabyFace} from 'react-icons/gi'
 import styles from '../styles/Nav.module.css'
 
 const Nav = ({ show, onNavClose }) => {
+  const router = useRouter()
   return (
     <div>
       <div className={`nav-panel ${show ? 'active' : ''} ${styles.navInfo}`}>
-        <GiBabyFace className={styles.logo} size="5rem" />
-        <div className={styles.navInfoName}>台灣旅遊景點導覽地圖</div>
-        <div>資料介接「交通部PTX平臺」</div>
-        <a href="https://ptx.transportdata.tw/PTX/" target="_blank" rel="noreferrer"><img src="/images/PTX_logo.png" alt="資料介接「交通部PTX平臺」" /></a>
+
+        <header>
+          <GiBabyFace className={styles.logo} size="5rem" />
+          <h1>台灣旅遊景點導覽地圖</h1>
+        </header>
+
+        <main>
+          <div>資料介接「交通部PTX平臺」</div>
+          <a href="https://ptx.transportdata.tw/PTX/" target="_blank" rel="noopener noreferrer">
+            <img src={`${router.basePath}/images/PTX_logo.png`} alt="資料介接「交通部PTX平臺」" />
+          </a>
+        </main>
+
+        <footer>
+          <div>ver: {process.env.APP_VERSION}</div>
+        </footer>
       </div>
+
       <div className={`screen-mask ${show ? 'active' : ''}`} onClick={onNavClose}/>
     </div>
   )
--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   reactStrictMode: true,
   env: {
+    APP_VERSION: process.env.APP_VERSION,
     MAPBOX_TOKEN: process.env.REACT_APP_MAP_API,
     PTX_API_ID: process.env.PTX_API_ID,
     PTX_API_KEY: process.env.PTX_API_KEY,
   },
+  basePath: '/taiwan-tourist-attraction-map',
 }
