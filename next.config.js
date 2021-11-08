a8030722905cddf7b50ffdc3926e126a55055d3a
ahung888 <yunhung2000@gmail.com>
2021-11-08 22:29:05 +0800

    try deploy

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    APP_VERSION: '0.1.4',
+    APP_VERSION: '0.1.5',
     MAPBOX_TOKEN: process.env.REACT_APP_MAP_API,
-    PTX_API_ID: process.env.PTX_API_ID,
-    PTX_API_KEY: process.env.PTX_API_KEY,
+    PTX_API_ID: process.env.REACT_APP_PTX_API_ID,
+    PTX_API_KEY: process.env.REACT_APP_PTX_API_KEY,
   },
   basePath: '/taiwan-tourist-attraction-map',
 }
