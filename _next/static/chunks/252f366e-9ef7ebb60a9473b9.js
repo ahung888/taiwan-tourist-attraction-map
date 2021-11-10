bb57f1b472fcc67ee0085e8e9c33c5acf0b7d681
ahung888 <ahung888@users.noreply.github.com>
2021-11-10 11:00:18 +0000

    deploy: a132681d9ca01bb7c1dcbcf42ebbd8df5a9b09c8

--- _next/static/chunks/252f366e-9ef7ebb60a9473b9.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[228],{5434:function(t,n,a){a.d(n,{rWj:function(){return e}});var r=a(8357);function e(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z"}}]})(t)}}}]);
